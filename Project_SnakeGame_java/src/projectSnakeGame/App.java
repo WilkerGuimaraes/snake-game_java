73cc7c74ca7a6fe9028454f303a7e401bfab0e21
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2023-11-05 17:19:07 -0300

    commit 1.0

---
+++ Project_SnakeGame_java/src/projectSnakeGame/App.java
@@ -0,0 +1,22 @@
+package projectSnakeGame;
+
+import javax.swing.*;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+        int boardWidth = 1200;
+        int boardHeight = 900;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+        snakeGame.requestFocus();
+    }
+}
